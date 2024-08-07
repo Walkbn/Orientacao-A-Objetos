1cffa35db39400527952ea43ee8e97717e8008dc
Walkbn <Walk2233bueno1@gmail.com>
2024-08-07 20:54:24 -0300

    Update CalculoDeAreas.java

--- CalculoDeAreas.java
+++ CalculoDeAreas.java
@@ -1,7 +1,8 @@
+import java.lang.Math;
 import java.util.Scanner;
 public class CalculoDeAreas {
     public static void main(String[] args) {
-        int menu = 0; double areaRetangulo = 0, areaCirculo = 0, areaTriangulo = 0, raioRetangulo = 0, raioCirculo = 0, raioTriangulo = 0;
+        int menu = 0; 
         Scanner scan = new Scanner(System.in);
         while (menu==0) {
             System.out.println("Selecione a opção correspondente: ");
@@ -9,26 +10,44 @@ public class CalculoDeAreas {
             System.out.println("2 - Área do Retângulo.");
             System.out.println("3- Área do Triângulo.");
             System.out.println("4 - Sair.");
-            System.out.println("\n->");
+            System.out.print("\n->");
+            menu = scan.nextInt();
             switch (menu) {
-                case 1: 
-                circulo(areaCirculo, raioCirculo);
-                case 2:
-                retangulo(areaRetangulo, areaRetangulo);
-                case 3:
-                triangulo(areaTriangulo, areaTriangulo);
-                default: System.out.println("Digite uma opção coerente: ");
+                case 1: circulo(); break;
+                case 2: retangulo(); break;
+                case 3: triangulo(); break;
+                case 4: menu++; break;
+                default: System.out.println("Digite uma opção correta. . .\n"); break;
             }
             scan.close();
         }
     }
-    public static double circulo(double areaCirculo, double raioCirculo) {
-
+    public static void circulo() {
+        System.out.print("Digite o valor do raio: ");
+        Scanner scanout = new Scanner(System.in);
+        double raio = scanout.nextDouble();
+        scanout.close();
+        double resultadoCirculo = Math.PI*(raio*raio);
+        System.out.println(resultadoCirculo);
         }
-    public static double retangulo(double areaRetangulo, double raioRetangulo) {
-
+    public static void retangulo() {
+        System.out.print("Digite a altura: ");
+        Scanner scanout = new Scanner(System.in);
+        double altura = scanout.nextDouble();
+        System.out.print("Digite a largura: ");
+        double largura = scanout.nextDouble();
+        scanout.close();
+        double resultadoRetangulo = altura*largura;
+        System.out.println(resultadoRetangulo);
     }
-    public static double triangulo(double areaTriangulo, double raioTriangulo) {
-
+    public static void triangulo() {
+        System.out.print("Digite o tamanho da base do triângulo: ");
+        Scanner scanout = new Scanner(System.in);
+        double base = scanout.nextDouble();
+        System.out.println("Digite a altura: ");
+        double altura = scanout.nextDouble();
+        double resultadoTriangulo = (base*altura)/2;
+        System.out.println(resultadoTriangulo);
+        scanout.close();
     }
 }
